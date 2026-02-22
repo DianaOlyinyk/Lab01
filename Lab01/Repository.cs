0e8c1d146e6dc2fcf579b98ab09f9fb4bf10d2f9
DianaOlyinyk <di.oliinyk@ukma.edu.ua>
2026-02-22 12:34:08 +0200

    Repository added

---
+++ Lab01/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    public static class Repository
+    {
+        private static List<Project> _projects; 
+        static Repository()
+        {
+            _projects = new List<Project> { new Project("Project1", "Exam preparation", ProjectType.Educational) };
+         
+        }
+    }
+}
